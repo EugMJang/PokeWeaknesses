f98e7e1e5c5ab873bbe21c2f3e2795daf9849d1e
Eugene Jang <eug.jang123@gmail.com>
2021-07-12 23:33:19 -0500

    Display strengths, weaknesses, and immunities on popup window

--- pokemon-types.js
+++ pokemon-types.js
@@ -3,30 +3,49 @@ async function get_pokemon_weaknesses(request) {
     if (response.status != 200) {
         response = await fetch(`https://pokeapi.co/api/v2/pokemon/${request.pokemon.toLowerCase()}-standard`); // Try putting -standard if the request doesnt work
     }
-    let weaknesses = new Map()
-    if (response.status == 200) {
-        const json = await response.json(); 
-        const pokemon_types = json.types;
+    // If that fails, return
+    if (response.status != 200) {
+        return;
+    }
 
-        // Iterate through pokemon's types and extract strengths/weaknesses
-        for (let i = 0; i < pokemon_types.length; i++) {
-            const type_response = await fetch(pokemon_types[i].type.url).then(response => response.json());
-            const damage_relations = type_response.damage_relations;
+    let multipliers = new Map()
+    const json = await response.json(); 
+    const pokemon_types = json.types;
 
-            update_weaknesses_map(weaknesses, damage_relations);
+    // Iterate through pokemon's types and extract strengths/weaknesses
+    for (let i = 0; i < pokemon_types.length; i++) {
+        const type_response = await fetch(pokemon_types[i].type.url).then(response => response.json());
+        const damage_relations = type_response.damage_relations;
+
+        update_multipliers(multipliers, damage_relations);
+    }
+
+    // Separate multipliers into strengths, weaknesses, and immunities
+    let strengths = [];
+    let weaknesses = [];
+    let immunities = [];
+    for (let [type, multiplier] of multipliers) {
+        if (multiplier > 1) {
+            weaknesses.push(type);
+        } else if (multiplier == 0) {
+            immunities.push(type);
+        } else if (multiplier < 1) {
+            strengths.push(type);
         }
     }
+
+    return [strengths, weaknesses, immunities];
 }
 
-function update_weaknesses_map(weaknesses, damage_relations) {
+function update_multipliers(multipliers, damage_relations) {
     // Iterate through types that deal double damage to pokemon
     const double_damage = damage_relations.double_damage_from;
     for (let j = 0; j < double_damage.length; j++) {
         // Multiply by 2 if the type is in map, otherwise set key, value to type, 2
-        if (weaknesses.has(double_damage[j].name)) {
-            weaknesses.set(double_damage[j].name, weaknesses.get(double_damage[j].name) * 2);
+        if (multipliers.has(double_damage[j].name)) {
+            multipliers.set(double_damage[j].name, multipliers.get(double_damage[j].name) * 2);
         } else {
-            weaknesses.set(double_damage[j].name, 2.0)
+            multipliers.set(double_damage[j].name, 2.0)
         }
     }
 
@@ -34,24 +53,17 @@ function update_weaknesses_map(weaknesses, damage_relations) {
     const half_damage = damage_relations.half_damage_from;
     for (let i = 0; i < half_damage.length; i++) {
         // Divide by 2 if the type is in map, otherwise set key, value to type, 0.5
-        if (weaknesses.has(half_damage[i].name)) {
-            weaknesses.set(half_damage[i].name, weaknesses.get(half_damage[i].name) / 2);
+        if (multipliers.has(half_damage[i].name)) {
+            multipliers.set(half_damage[i].name, multipliers.get(half_damage[i].name) / 2);
         } else {
-            weaknesses.set(half_damage[i].name, 0.5);
+            multipliers.set(half_damage[i].name, 0.5);
         }
     }
 
     // Iterate through types that do no damage to pokemon
     const no_damage = damage_relations.no_damage_from;
     for (let i = 0; i < no_damage.length; i++) {
-        weaknesses.set(no_damage[i].name, 0);
-    }
-
-    // Iterate through map; if value is 1, remove
-    for (let [key, value] of weaknesses) {
-        if (value == 1) {
-            weaknesses.delete(key);
-        }
+        multipliers.set(no_damage[i].name, 0);
     }
 }
 
--- popup.js
+++ popup.js
@@ -1,11 +1,46 @@
 import {get_pokemon_weaknesses} from "./pokemon-types.js"
 
 chrome.runtime.onMessage.addListener((request) => {
-    const pokemon = document.getElementById(request.msg);
-    if (request.pokemon != null) {
+    const pokemon = document.getElementById(request.msg); // Could be left side or right side depending on pokemon
+    if (request.pokemon != null && pokemon.innerHTML == "") {
         pokemon.innerHTML = "<h1>" + request.pokemon + "</h1>";
-        let weaknesses = get_pokemon_weaknesses(request);
-    } else {
-        pokemon.innerHTML = "<h1>None<h1>";
+        get_pokemon_weaknesses(request).then(stats => display_pokemon_stats(pokemon, stats)); // 2d array containing strengths, weaknesses, and immunities
+    } else if (request.pokemon == null) {
+        pokemon.innerHTML = "";
     }
 });
+
+function display_pokemon_stats(screen, stats) {
+    // Display strengths
+    screen.innerHTML += "<p>Strengths: ";
+    if (stats[0].length != 0) {
+        for (let i = 0; i < stats[0].length - 1; i++) {
+            screen.innerHTML += stats[0][i] + ", ";
+        }
+        screen.innerHTML += stats[0][stats[0].length - 1] + "</p>"
+    } else {
+        screen.innerHTML += "None</p>";
+    }
+
+    // Display weaknesses
+    screen.innerHTML += "<p>Weaknesses: ";
+    if (stats[1].length != 0) {
+        for (let i = 0; i < stats[1].length - 1; i++) {
+            screen.innerHTML += stats[1][i] + ", ";
+        }
+        screen.innerHTML += stats[1][stats[1].length - 1] + "</p>"
+    } else {
+        screen.innerHTML += "None</p>";
+    }
+
+    // Display immunities
+    screen.innerHTML += "<p>Immunities: ";
+    if (stats[2].length != 0) {
+        for (let i = 0; i < stats[2].length - 1; i++) {
+            screen.innerHTML += stats[2][i] + ", ";
+        }
+        screen.innerHTML += stats[2][stats[2].length - 1] + "</p>"
+    } else {
+        screen.innerHTML += "None</p>"
+    }
+}
